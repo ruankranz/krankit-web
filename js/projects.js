20618ca70aff14155903f663af2bb22ab7b91a11
Ruan K <ruankranz@gmail.com>
2018-10-10 22:06:57 +0200

    cleanup

--- js/projects.js
+++ js/projects.js
@@ -1,13 +1,4 @@
-/**
- * demo1.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2018, Codrops
- * http://www.codrops.com
- */
+
 
 /**
  * Equation of a line.
